34885651b263c176cd5106179fd673f6bf3921c4
hargun13 <chandhokhargun@gmail.com>
2023-11-06 01:31:49 +0530

    cleanup commit

--- src/components/DeviceOrientationModal.js
+++ src/components/DeviceOrientationModal.js
@@ -11,7 +11,7 @@ function DeviceOrientationModal({ onClose, isMobile, orientation }) {
             <div className="bg-red-500 p-4 rounded-lg text-white text-center">
                 <p className="font-bold">Warning<br/><br/></p>
                 <p className="text-lg">
-                    You are using a mobile phone. <br/><br/>This game is best played and understood on a laptop or a PC setup. <br/><br/>vIf you are only viewing it, please turn your phone to landscape mode for the best viewing experience.
+                    You are using a mobile phone. <br/><br/>This game is best played and understood on a laptop or a PC setup. <br/><br/>If you are only viewing it, please turn your phone to landscape mode for the best viewing experience.
                 </p>
             </div>        
         ) : (
