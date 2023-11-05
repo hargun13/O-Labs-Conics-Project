31ca2fe3d611e689bfc8341696bc0c5f85ac21be
hargun13 <chandhokhargun@gmail.com>
2023-11-05 19:18:05 +0530

    project completed - left for final verification

--- src/components/Simulation.jsx
+++ src/components/Simulation.jsx
@@ -4,26 +4,25 @@ import Frame from './FrameNavbar'
 import Navbar from './Navbar'
 import {Unity, useUnityContext}  from 'react-unity-webgl'
 
-
-
 function Simulation() {
 
   const { unityProvider } = useUnityContext({
-    loaderUrl: "./Test_build_three/Build/Test_build_three.loader.js",
-    dataUrl: "./Test_build_three/Build/webgl.data",
-    frameworkUrl: "./Test_build_three/Build/build.framework.js",
-    codeUrl: "./Test_build_three/Build/build.wasm",
+    loaderUrl: "./New Build File/Build/New Build File.loader.js",
+    dataUrl: "./New Build File/Build/webgl.data",
+    frameworkUrl: "./New Build File/Build/build.framework.js",
+    codeUrl: "./New Build File/Build/build.wasm",
   });
+
 
   return (
     <div>
 
       <Navbar />
 
-      {/* Content for Theory page */}
       <div className='px-10 py-5 rounded-xl m-4 bg-white'>
             
       <Frame />
+      
       <div className='border-r-2 border-b-2 border-l-2 rounded-xl py-10 px-5'>
 
         <Unity style={{height:"auto", width:"100%"}} 
@@ -32,8 +31,6 @@ function Simulation() {
       </div>
 
       </div>
-      {/* ///////// */}
-
 
       <Footer />
 
